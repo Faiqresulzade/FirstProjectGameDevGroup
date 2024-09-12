aff8395df34e8d259cf4f9777cf6d08414e52f86
Faiqresulzade <faigbr@code.edu.az>
2024-09-12 20:51:19 +0400

    first commit

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿//Console.WriteLine("Salam Dunya");
+
+//reqemler 
+//int , float , double, uint ,decimal
+
+//herfler
+//string , char --string builder
+
+//int number = 10;
+//decimal f = 0.2M;
+//decimal f1 = 0.1M;
+//decimal d = 20;
+//double db = 30;
+//long l = 12;
+
+//Console.WriteLine(f * f1);
+
+string name = "Nicat";
+string surname = "Alioglu";
+int age = 17;
+
+//Console.WriteLine(name + " " + surname+ " " +age);
+
+//Console.WriteLine($"{name} {surname} {age}");
+
+//bool
+
+//bool isOkey = true;
+
+
+int b = 2 + 2;
+int c = 2 * 2;
+int d = 2 / 2;
+int e = 2 - 2;
+
+bool s = 4 == 5;
+Console.WriteLine(s);
